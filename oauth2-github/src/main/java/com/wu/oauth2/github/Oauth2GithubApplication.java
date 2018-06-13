2cb40cfe4466238f76e85f7c7e1bd3e2b2ffac10
wu05281 <37809131@qq.com>
2018-06-13 15:48:06 +0800

    done！

--- oauth2-github/src/main/java/com/wu/oauth2/github/Oauth2GithubApplication.java
+++ oauth2-github/src/main/java/com/wu/oauth2/github/Oauth2GithubApplication.java
@@ -3,17 +3,29 @@ package com.wu.oauth2.github;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import javax.servlet.Filter;
 import java.security.Principal;
 
 /**
  * @author alex
- * 问题：http配置详细解释；
+ *         问题：http配置详细解释；
  */
 @SpringBootApplication
 @EnableOAuth2Sso
@@ -21,26 +33,59 @@ import java.security.Principal;
 public class Oauth2GithubApplication extends WebSecurityConfigurerAdapter {
 
 
-	@RequestMapping("/user")
-	public Principal user(Principal principal) {
-		return principal;
-	}
+    @Resource
+    private AuthorizationCodeResourceDetails details;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-				.antMatcher("/**")
-				.authorizeRequests()
-					.antMatchers("/", "/login**", "/webjars/**", "/error**")
-					.permitAll()
-				.anyRequest()
-					.authenticated()
-				.and().logout().logoutSuccessUrl("/").permitAll()
-				.and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-	}
+    @Resource
+    private ResourceServerProperties props;
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(Oauth2GithubApplication.class, args);
-	}
+    @Resource
+    private OAuth2ClientContext oauth2ClientContext;
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                //通过 authorizeRequests() 定义哪些URL需要被保护、哪些不需要被保护。
+                //这里指定了 / 和 /login** /webjars/**  /error**不需要任何认证就可以访问
+                //其他的路径都必须通过身份验证。
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+                .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+    }
+
+    private Filter ssoFilter() {
+        OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/github");
+        OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(details, oauth2ClientContext);
+        facebookFilter.setRestTemplate(facebookTemplate);
+        UserInfoTokenServices tokenServices = new UserInfoTokenServices(props.getUserInfoUri(), details.getClientId());
+        tokenServices.setRestTemplate(facebookTemplate);
+        facebookFilter.setTokenServices(tokenServices);
+        return facebookFilter;
+    }
+
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(
+            OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2GithubApplication.class, args);
+    }
 }
